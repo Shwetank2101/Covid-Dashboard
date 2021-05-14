e1825964b59680512bde49ba04d2746189e756e2
Shwetank2101 <shwetankdixitofficial@gmail.com>
2021-05-14 17:25:54 +0530

    update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
