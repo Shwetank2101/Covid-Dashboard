e82c1eca064856987113ed5758e089c5b0bfbe58
Sameeksha agarwal <sameeksha.agarwal12@gmail.com>
2021-05-16 23:44:31 +0530

    home page

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,10 +7,9 @@ from django.views.decorators.csrf import csrf_exempt
 import requests
 
 
-
 # Create your views here.
-# def index(request):
-# 	return render(request,'index.html')
+def home(request):
+    return render(request, 'index.html')
 
 # def covid(request):
 # 	# data=requests.get('https://api.covid19api.com/countries').json
@@ -21,46 +20,46 @@ import requests
 # 	return render(request,'covidstat.html',context)
 
 
-
 def index(request):
 
-  response = requests.get('https://api.covid19india.org/data.json')
-  resp = response.json()
+    response = requests.get('https://api.covid19india.org/data.json')
+    resp = response.json()
 
- # India count
+   # India count
 
-  statewise = resp['statewise']
+    statewise = resp['statewise']
 
-  india = statewise[0]
+    india = statewise[0]
 
-  total_india = india['confirmed']
+    total_india = india['confirmed']
 
-  active_india = india['active']
+    active_india = india['active']
 
-  recovered_india = india['recovered']
+    recovered_india = india['recovered']
 
-  dead_india = india['deaths']
+    dead_india = india['deaths']
 
-  updated_time_india = india['lastupdatedtime']
+    updated_time_india = india['lastupdatedtime']
 
-  #Uttar Pradesh count
+    # Uttar Pradesh count
 
-  i=0
-  ind = ''
-  for k in statewise:
-      if k['state'] == 'Uttar Pradesh':
-          ind = i
-      i = i + 1
+    i = 0
+    ind = ''
+    for k in statewise:
+        if k['state'] == 'Uttar Pradesh':
+            ind = i
+        i = i + 1
 
-  uttarpradesh = statewise[ind]
+    uttarpradesh = statewise[ind]
 
-  total_uttarpradesh = {'total':uttarpradesh['confirmed']}
+    total_uttarpradesh = {'total': uttarpradesh['confirmed']}
 
-  active_uttarpradesh = {'active':uttarpradesh['active']}
+    active_uttarpradesh = {'active': uttarpradesh['active']}
 
-  recovered_uttarpradesh = {'recovered':uttarpradesh['recovered']}
+    recovered_uttarpradesh = {'recovered': uttarpradesh['recovered']}
 
-  dead_uttarpradesh = {'dead':uttarpradesh['deaths']}
-  context={'a':[total_uttarpradesh,active_uttarpradesh,recovered_uttarpradesh,dead_uttarpradesh],'name':['total','active','recovered','dead']}
+    dead_uttarpradesh = {'dead': uttarpradesh['deaths']}
+    context = {'a': [total_uttarpradesh, active_uttarpradesh, recovered_uttarpradesh,
+                     dead_uttarpradesh], 'name': ['total', 'active', 'recovered', 'dead']}
 
-  return render(request,'covidstat.html',context)
+    return render(request, 'covidstat.html', context)
