c5ab9b72ca8c3138c7705f74f05bc65dfae1f4b7
Shwetank2101 <shwetankdixitofficial@gmail.com>
2021-06-24 08:04:48 +0530

    update data

--- dashboard/views.py
+++ dashboard/views.py
@@ -43,10 +43,18 @@ def index(request):
     updated_time_india = india['lastupdatedtime']
     
     state=['Total', 'Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Dadra and Nagar Haveli and Daman and Diu', 'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Ladakh', 'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'State Unassigned', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal']
-    check={}
+    check=[]
     for i in range(1,len(state)):
-        check[state[i]] = {'total':statewise[i]['confirmed'],'active':statewise[i]['active'],'recovered':statewise[i]['recovered'],'death':statewise[i]['deaths']}
+        if state[i]=='State Unassigned':
+            continue
+        # check[state[i]] = {'total':statewise[i]['confirmed'],'active':statewise[i]['active'],'recovered':statewise[i]['recovered'],'death':statewise[i]['deaths']}
+        check.append([state[i],{'confirmed':statewise[i]['confirmed'],'active':statewise[i]['active'],'recovered':statewise[i]['recovered'],'deaths':statewise[i]['deaths']}])
+    
+
         
+        #check.append([state[i],[statewise[i]['confirmed'],statewise[i]['active'],statewise[i]['recovered'],statewise[i]['deaths']]])
+    
     context={'name': ['total', 'active', 'recovered', 'dead'],'states':check}
+
 
     return render(request, 'covidstat.html', context)
