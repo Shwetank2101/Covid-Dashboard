4e7d0832b4aedc11210afefa3102eecce6cb9374
Shwetank2101 <shwetankdixitofficial@gmail.com>
2021-06-24 10:55:44 +0530

    update data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('data', views.index, name='index'),
+    path('state',views.state,name='state'),
+    path('city',views.city,name='city'),
     path('', views.home, name='home')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -47,14 +47,63 @@ def index(request):
     for i in range(len(state)):
         if state[i]=='State Unassigned':
             continue
-        # check[state[i]] = {'total':statewise[i]['confirmed'],'active':statewise[i]['active'],'recovered':statewise[i]['recovered'],'death':statewise[i]['deaths']}
         check.append([state[i],{'confirmed':statewise[i]['confirmed'],'active':statewise[i]['active'],'recovered':statewise[i]['recovered'],'deaths':statewise[i]['deaths']}])
-    
 
-        
-        #check.append([state[i],[statewise[i]['confirmed'],statewise[i]['active'],statewise[i]['recovered'],statewise[i]['deaths']]])
-    
-    context={'name': ['total', 'active', 'recovered', 'dead'],'states':check}
-
+    context={'name': ['total', 'active', 'recovered', 'dead'],'states':check,'city':uttarpradesh_district,'statewise':tn_districts}
 
     return render(request, 'covidstat.html', context)
+
+def state(request):
+    d={}
+    response = requests.get('https://api.covid19india.org/data.json')
+    resp = response.json()
+    statewise = resp['statewise']
+    if request.method== 'POST':
+        value=request.POST.get('city')
+        i=0
+        ind = ''
+        for k in statewise:
+            if k['state'] == value:
+                ind = i
+                break
+            i = i + 1
+        # value=statewise[i]
+        value={'confirmed':statewise[i]['confirmed'],'active':statewise[i]['active'],'recovered':statewise[i]['recovered'],'deaths':statewise[i]['deaths']}
+        d={'city':value}
+    
+    return render(request,'state.html',d)
+
+def city(request):
+    d={}
+    if request.method== 'POST':
+        value=request.POST.get('city')
+        districtwise = requests.get('https://api.covid19india.org/state_district_wise.json')
+        districtwise = districtwise.json()
+        tn_districts = districtwise[value]['districtData']
+        t=tn_districts
+        key=0
+        final={}
+        ar=list(t.keys())
+        temp=0
+        while(len(list(t.keys()))!=0):
+            key=0
+            ar=list(t.keys())
+            temp=0
+            for i in range(len(ar)-1):
+                for j in range(i+1,len(ar)):
+                    #print(t[ar[i]]['confirmed'])
+                    if t[ar[i]]['confirmed']>temp:
+                        temp=t[ar[i]]['confirmed']
+                        key=i
+            final[ar[key]]=t[ar[key]]
+            del t[ar[key]]
+        tn_districts=final
+        value=tn_districts
+        d={'city':value}
+    
+    
+    return render(request,'city.html',d)
+
+
+
+    
